8381e9de2f1b290a0c6081abb7e893e5015f6e0a
leobhmg45 <leobhmg45@gmail.com>
2022-12-21 17:08:30 -0300

    tailwind inserido

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,9 @@
-// import { Container } from './components/Container'
-// import { MyButton } from './components/MyButton'
-
-//import {Container, MyButton} from './AppStyles'
-import * as C from './AppStyles'
 const App = () => {
 
   return (
-    <C.Container bgColor='#000000'>
-      My Container
-      <C.MyButton>Button 1</C.MyButton>
-      <C.MyButton small>Button 2</C.MyButton>
-    </C.Container>
+    <div className="text-3xl font-bold underline">
+      My text
+    </div>
   )
 }
 
